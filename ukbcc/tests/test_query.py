f582b64d42b954b7410fc7e0ccf17d21f193ad28
Nathalie Willems <nathalie.willems@ibm.com>
2020-09-24 15:14:46 +1000

    added automated testing to travis yml

--- ukbcc/tests/test_query.py
+++ ukbcc/tests/test_query.py
@@ -1,5 +1,5 @@
 import pytest
-from ukbcc import query 
+from ukbcc import query
 
 
 def test_basic_query_creation(main_csv, gp_csv):
@@ -20,7 +20,7 @@ def test_all_query_basic(main_csv, gp_csv):
                                 queries=gen_query, main_filename=main_csv,
                                 write_dir=None, gpc_path=gp_csv,
                                 out_filename=None, write=False)
-    
+
     exp_ids = set(["1041796", "1037058", "1024938", "1016017", "1038882", "1030520", "1003670", "1027017"])
     assert exp_ids == set(ids)
 
@@ -35,7 +35,7 @@ def test_all_multiple_columns(main_csv, gp_csv):
                                 queries=gen_query, main_filename=main_csv,
                                 write_dir=None, gpc_path=gp_csv,
                                 out_filename=None, write=False)
-    
+
     exp_ids = set(["1037918",  "1033149",  "1016017",  "1033388",  "1030520",  "1003670"])
     assert exp_ids == set(ids)
 
@@ -50,7 +50,7 @@ def test_any_singleton_query(main_csv, gp_csv):
                                 queries=gen_query, main_filename=main_csv,
                                 write_dir=None, gpc_path=gp_csv,
                                 out_filename=None, write=False)
-    
+
     exp_ids = set(["1041796", "1037058", "1024938", "1016017", "1038882", "1030520", "1003670", "1027017"])
     assert exp_ids == set(ids)
 
@@ -67,7 +67,7 @@ def test_any_multiple_columns(main_csv, gp_csv):
                                 queries=gen_query, main_filename=main_csv,
                                 write_dir=None, gpc_path=gp_csv,
                                 out_filename=None, write=False)
-    
+
     exp_ids = set(["1037918",  "1033149",  "1016017",  "1033388",  "1030520",  "1003670"])
     assert exp_ids == set(ids)
 
@@ -82,7 +82,7 @@ def test_any_pair_same_field_query(main_csv, gp_csv):
                                 queries=gen_query, main_filename=main_csv,
                                 write_dir=None, gpc_path=gp_csv,
                                 out_filename=None, write=False)
-    
+
     exp_ids = set(["1037918", "1041796", "1037058", "1024938", "1016017", "1038882", "1030520", "1003670", "1027017"])
     assert exp_ids == set(ids)
 
@@ -97,7 +97,7 @@ def test_any_pair_diff_field_query(main_csv, gp_csv):
                                 queries=gen_query, main_filename=main_csv,
                                 write_dir=None, gpc_path=gp_csv,
                                 out_filename=None, write=False)
-    
+
     exp_ids = set(["1041796", "1037058", "1024938", "1016017", "1038882", "1030520", "1003670", "1027017"])
     assert exp_ids == set(ids)
 
@@ -113,7 +113,7 @@ def test_none_basic(main_csv, gp_csv):
                                 queries=gen_query, main_filename=main_csv,
                                 write_dir=None, gpc_path=gp_csv,
                                 out_filename=None, write=False)
-    
+
     print(ids)
     exp_ids = set(["1037918",  "1033149",  "1033388",  "1031625",  "1031595",  "1008947"])
     assert exp_ids == set(ids)
@@ -129,12 +129,12 @@ def test_all__none_multiple_columns(main_csv, gp_csv):
                                 queries=gen_query, main_filename=main_csv,
                                 write_dir=None, gpc_path=gp_csv,
                                 out_filename=None, write=False)
-    
+
     exp_ids = set([ "1033149",  "1016017",  "1033388",  "1030520",  "1003670"])
     assert exp_ids == set(ids)
 
 
-#Check that "none_of" returns empty fields. 
+#Check that "none_of" returns empty fields.
 def test_none_returns_empty(main_csv, gp_csv):
     cohort_criteria = {'all_of': [('6148', "4")], 'any_of': [], 'none_of': [("6070", "2"), ("6070", "1")]}
     gen_query = query.create_queries(cohort_criteria,
@@ -144,7 +144,7 @@ def test_none_returns_empty(main_csv, gp_csv):
                                 queries=gen_query, main_filename=main_csv,
                                 write_dir=None, gpc_path=gp_csv,
                                 out_filename=None, write=False)
-    
+
     exp_ids = set(["1033149",  "1033388"])
     assert exp_ids == set(ids)
 
@@ -160,7 +160,7 @@ def test_fields_with_spaces(main_csv, gp_csv):
                                 queries=gen_query, main_filename=main_csv,
                                 write_dir=None, gpc_path=gp_csv,
                                 out_filename=None, write=False)
-    
+
     exp_ids = set(["1033149",  "1041796"])
     assert exp_ids == set(ids)
 
@@ -175,7 +175,7 @@ def test_missing_fields(main_csv, gp_csv):
                                 queries=gen_query, main_filename=main_csv,
                                 write_dir=None, gpc_path=gp_csv,
                                 out_filename=None, write=False)
-    
+
     exp_ids = set([])
     assert exp_ids == set(ids)
 
@@ -189,13 +189,13 @@ def test_missing_fields(main_csv, gp_csv):
                                 queries=gen_query, main_filename=main_csv,
                                 write_dir=None, gpc_path=gp_csv,
                                 out_filename=None, write=False)
-    
+
     exp_ids = set([])
     assert exp_ids == set(ids)
 
 
 #Can we do a query on read2 at all?
-def test_all_gp_clinical_read2(main_csv, gp_csv): 
+def test_all_gp_clinical_read2(main_csv, gp_csv):
     cohort_criteria = {'all_of': [('read_2', "229..")], 'any_of': [], 'none_of': []}
     gen_query = query.create_queries(cohort_criteria,
                                    main_filename=main_csv,
@@ -204,13 +204,13 @@ def test_all_gp_clinical_read2(main_csv, gp_csv):
                                 queries=gen_query, main_filename=main_csv,
                                 write_dir=None, gpc_path=gp_csv,
                                 out_filename=None, write=False)
-    
+
     exp_ids = set(["1037918"])
     assert exp_ids == set(ids)
 
 
 #Can we read read2 when multiple entries per person
-def test_all_gp_clinical_read_2_multiple(main_csv, gp_csv): 
+def test_all_gp_clinical_read_2_multiple(main_csv, gp_csv):
     cohort_criteria = {'all_of': [('read_2', "XE0of")], 'any_of': [], 'none_of': []}
     gen_query = query.create_queries(cohort_criteria,
                                    main_filename=main_csv,
@@ -219,13 +219,13 @@ def test_all_gp_clinical_read_2_multiple(main_csv, gp_csv):
                                 queries=gen_query, main_filename=main_csv,
                                 write_dir=None, gpc_path=gp_csv,
                                 out_filename=None, write=False)
-    
+
     exp_ids = set(["1016017"])
     assert exp_ids == set(ids)
 
 
 #Can we read read3
-def test_all_gp_clinical_read_3(main_csv, gp_csv): 
+def test_all_gp_clinical_read_3(main_csv, gp_csv):
     cohort_criteria = {'all_of': [('read_3', "XE0Gu")], 'any_of': [], 'none_of': []}
     gen_query = query.create_queries(cohort_criteria,
                                    main_filename=main_csv,
@@ -234,12 +234,12 @@ def test_all_gp_clinical_read_3(main_csv, gp_csv):
                                 queries=gen_query, main_filename=main_csv,
                                 write_dir=None, gpc_path=gp_csv,
                                 out_filename=None, write=False)
-    
+
     exp_ids = set(["1037918"])
     assert exp_ids == set(ids)
 
 #Can we query 2 and 3?
-def test_all_gp_clinical_read2and3(main_csv, gp_csv): 
+def test_all_gp_clinical_read2and3(main_csv, gp_csv):
     cohort_criteria = {'all_of': [], 'any_of': [('read_2', "XE0of"), ('read_3', 'XE0Gu')],  'none_of': []}
     gen_query = query.create_queries(cohort_criteria,
                                    main_filename=main_csv,
@@ -248,7 +248,7 @@ def test_all_gp_clinical_read2and3(main_csv, gp_csv):
                                 queries=gen_query, main_filename=main_csv,
                                 write_dir=None, gpc_path=gp_csv,
                                 out_filename=None, write=False)
-    
+
     exp_ids = set(["1016017", "1037918"])
     assert exp_ids == set(ids)
 
@@ -263,7 +263,6 @@ def test_any_pair_same_field_query(main_csv, gp_csv):
                                 queries=gen_query, main_filename=main_csv,
                                 write_dir=None, gpc_path=gp_csv,
                                 out_filename=None, write=False)
-    
+
     exp_ids = set(["1041796", "1037058",  "1030520",  "1003670"])
     assert exp_ids == set(ids)
-
